e61eabe5051982be8c33c240c785cb472192a855
savarise <143745809+savarise@users.noreply.github.com>
2023-09-18 10:45:40 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def compter_mots(chaine):
+    # split() permet de separer une phrase en chaines de characteres en les separant par un espace (whitespace)
+    # on utilise donc le chaine (ce que on met sur la fonction) et on le split()
+    mots = chaine.split()
+
+    # on utlise la fonction len() pour compter le nombre de chaines de characteres qu'on a dans le chaine.split()
+    nombre_de_mots = len(mots)
+
+    print("Le nombre de mots dans la chaine est :", nombre_de_mots)
+# alors, on utilise la fonction sur ce que l'utilisateur met comme input
+chaine_de_caracteres = input()
+compter_mots(chaine_de_caracteres)
